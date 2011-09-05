a540bf4e137c57f60a9c49737506280ecc211d98
Kjel Delaey <kjel_delaey@hotmail.com>
2011-09-05 15:52:54 +0200

    Fix controller method 'session_captcha_valid?': encrypt the user input and compare it with the value stored in the session + remove documentation

--- lib/session_captcha/controller.rb
+++ lib/session_captcha/controller.rb
@@ -1,28 +1,13 @@
 module SessionCaptcha #:nodoc
   module ControllerHelpers #:nodoc
-    # This method is to validate the simple captcha in controller.
-    # It means when the captcha is controller based i.e. :object has not been passed to the method show_session_captcha.
-    #
-    # *Example*
-    #
-    # If you want to save an object say @user only if the captcha is validated then do like this in action...
-    #
-    #  if session_captcha_valid?
-    #   @user.save
-    #  else
-    #   flash[:notice] = "captcha did not match"
-    #   redirect_to :action => "myaction"
-    #  end
     def session_captcha_valid?
       return true if Rails.env.test?
       
       if params[:captcha]
-        data = SessionCaptcha::Utils::session_captcha_value(session[:captcha])
-        result = data == params[:captcha].delete(" ").upcase
-        SessionCaptcha::Utils::session_captcha_passed!(session[:captcha]) if result
-        return result
+        hashed_value = SessionCaptcha::Utils::generate_key( session[:session_id], params[:captcha].delete(" ").upcase )
+        session[:captcha] == hashed_value
       else
-        return false
+        false
       end
     end
 
