ac8f3ab20acc9a4f67258ac8932e21fd40f5d495
Kjel Delaey <kjel_delaey@hotmail.com>
2011-09-06 12:45:58 +0200

    Add session_captcha.rb initializer file

---
+++ lib/generators/templates/session_captcha.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Make sure the salt is at least 64 characters and all random,
+# No regular words or you'll be exposed to dictionary attacks.
+
+#Rails.application.config.session_captcha_salt = ''
+
